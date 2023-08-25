8e978cc45d73591c81d95d3ad9f89a4d4dbcf19c
Luiz Fernando Ferreira <luizferreiracod3@gmail.com>
2023-08-24 23:12:42 -0300

    :refactor: refatorado conhecimentos.html

--- src/app/pages/section/conhecimentos/conhecimentos.component.ts
+++ src/app/pages/section/conhecimentos/conhecimentos.component.ts
@@ -21,4 +21,36 @@ export class ConhecimentosComponent implements OnInit  {
     this.showIcons = !this.showIcons;
     this.scrollReavealService.init();
   }
+
+  experiencias = [
+    { imagem : '../../../../assets/icons/angular.svg', title : 'Angular', alt : 'Icone do Angular'},
+    { imagem : '../../../../assets/icons/tsIcon.svg', title : 'TypeScript', alt : 'Icone do TypeScript'},
+    { imagem : '../../../../assets/icons/icons8-css.svg', title : 'CSS', alt : 'Icone do CSS'},
+    { imagem : '../../../../assets/icons/icons8-html.svg', title : 'HTML', alt : 'Icone do HTML'},
+    { imagem : '../../../../assets/icons/icons8-js.svg', title : 'Css', alt : 'Icone do CSS'},
+  ]
+
+
+  ferramentas = [
+    { imagem : '../../../../assets/icons/icons8-git.svg', title : 'Git', alt : 'Icone do Git'},
+    { imagem : '../../../../assets/icons/visualStudioCode.svg', title : 'VS Code', alt : 'Icone do Visual Studio Code'},
+    { imagem : '../../../../assets/icons/icons8-github.svg', title : 'GitHub', alt : 'Icone do GitHub'},
+  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
