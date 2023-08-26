55813ac0773ff3ef5f5b9ea0b14398990cb20791
Luiz Fernando Ferreira <luizferreiracod3@gmail.com>
2023-08-26 10:20:12 -0300

    :refactor: atualizações de layout e adicionado responsividade no editor do ace editor

--- src/app/pages/home/code-editor/editor.service.ts
+++ src/app/pages/home/code-editor/editor.service.ts
@@ -1,38 +1,57 @@
 import { Injectable } from '@angular/core';
-
 import * as ace from 'ace-builds';
 import 'ace-builds/src-noconflict/mode-typescript';
 import 'ace-builds/src-noconflict/theme-dracula';
-
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditorService {
+  constructor() { }
 
-constructor() { }
-
-  createEditor(elementId : string ) : ace.Ace.Editor {
+  createEditor(elementId: string): ace.Ace.Editor {
     const editor = ace.edit(elementId);
     editor.getSession().setMode(`ace/mode/typescript`);
     editor.setTheme('ace/theme/dracula');
+    this.setEditorOptions(editor);
+    this.Responsive(editor);
+    return editor;
+  }
+
+  private setEditorOptions(editor: ace.Ace.Editor) {
     editor.setOptions({
       fontSize: '20px',
-      fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace;',
-
+      fontFamily: 'ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace',
     });
     editor.renderer.setShowPrintMargin(false);
     editor.setOption('highlightActiveLine', false);
     editor.resize();
     editor.setReadOnly(true);
-    return editor;
+  }
+
+
+
+  private Responsive(editor: ace.Ace.Editor) {
+    const setEditorFontSize = () => {
+      const windowWidth = window.innerWidth;
+
+      if (windowWidth <= 600) {
+        editor.setFontSize('16px');
+      } else {
+        editor.setFontSize('20px');
+      }
+    };
+
+    setEditorFontSize();
+
+    window.addEventListener('resize', () => {
+      setEditorFontSize();
+    });
   }
 
   typingSpeed: number = 10;
 
-
-  simulateTyping(element: string, text: string ) {
+  simulateTyping(element: string, text: string) {
     const editorType = ace.edit(element);
     let currentIndex = 0;
     const textToType = `${text}`;
@@ -47,12 +66,4 @@ constructor() { }
     editorType.setReadOnly(true);
     editorType?.setValue('');
   }
-
-
 }
-
-
-
-
-
-
