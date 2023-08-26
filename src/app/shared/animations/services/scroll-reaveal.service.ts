e4fa889716ef0bdf966d0c44a9c34e9ee4c244e5
Luiz Fernando Ferreira <luizferreiracod3@gmail.com>
2023-08-26 18:55:10 -0300

    :fix: ajuste de layout

--- src/app/shared/animations/services/scroll-reaveal.service.ts
+++ src/app/shared/animations/services/scroll-reaveal.service.ts
@@ -17,11 +17,11 @@ export class ScrollReavealService {
       distance: '20px',
       duration: 800,
       delay: 200,
-      reset: true,
+      reset: false,
       easing: 'cubic-bezier(0.5, 0, 0, 1)',
       scale: 1,
       viewFactor: 0.3,
-      mobile: false,
+      mobile: true,
     });
 
     ScrollReveal().reveal('.scroll');
--- src/app/shared/components/cards/cards.component.ts
+++ src/app/shared/components/cards/cards.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScrollReavealService } from '../../animations/services/scroll-reaveal.service';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+
+  constructor(private scrollRevealService : ScrollReavealService) {}
+
+  ngOnInit(): void {
+    this.scrollRevealService.init();
+  }
 
   cards = [
     {site : 'PageSpeed Insights', desempenho : 'Excelente', link : 'https://pagespeed.web.dev/analysis/https-portifolio-lfzinn-vercel-app/4lltaccmah?form_factor=desktop'},
