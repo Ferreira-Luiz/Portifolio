280efc23891fedf626f5334e183c48c8cd47462c
Luiz Fernando Ferreira <luizferreiracod3@gmail.com>
2023-09-13 13:24:42 -0300

    :fix:

--- src/app/pages/home/components/code-editor/code-editor.component.ts
+++ src/app/pages/home/components/code-editor/code-editor.component.ts
@@ -5,9 +5,6 @@ import { slideInAnimation } from 'src/app/shared/services/modules/angularAnimati
 import { ScrollReavealService } from 'src/app/shared/services/animations/scroll-reaveal.service';
 import { EditorService } from './editor.service';
 
-
-
-
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
@@ -24,8 +21,6 @@ export class CodeEditorComponent implements AfterViewInit, OnInit {
     this.scrollReavealService.init();
 
 }
-
-
     editorHtml?: ace.Ace.Editor;
     editorCss?: ace.Ace.Editor;
     editorTs?: ace.Ace.Editor;
@@ -33,27 +28,15 @@ export class CodeEditorComponent implements AfterViewInit, OnInit {
     isHtmlOpen: boolean = false;
     isCssOpen: boolean = false;
 
-
-
-
-    ngAfterViewInit(): void {
+  ngAfterViewInit(): void {
       this.editorTs = this.editorService.createEditor('editorTs');
       this.editorService.simulateTyping('editorTs', `${this.dynamicTextTs}` );
+  }
 
-    }
-
-
-  dynamicTextTs: string = `import * as cssSkills from 'CSS'\nimport * as htmlSkills from 'HTML'\nimport * as tsSkills from 'TypeScript'\nimport * as jsSkills from 'JavaScript'\nimport * as softSkills from 'lifeExperiences'`;
-
-  dynamicTextHTML: string = `<!--Me mande uma Mensagem-->
-  <nav>\n<a href="https://www.linkedin.com/in/lfsilvaferreira/"> LinkedIn </a>\n</nav>`;
-
-
-  dynamicTextCss = `.luizFernando {\nidade : 23anos;\nexperiencia: 1.5anos;\ncargo : frontEndDeveloper;\n}`
-
-
-
-
+    dynamicTextTs: string = `import * as cssSkills from 'CSS'\nimport * as htmlSkills from 'HTML'\nimport * as tsSkills from 'TypeScript'\nimport * as jsSkills from 'JavaScript'\nimport * as softSkills from 'lifeExperiences'`;
+    dynamicTextHTML: string = `<!--Me mande uma Mensagem-->
+    <nav>\n<a href="https://www.linkedin.com/in/lfsilvaferreira/"> LinkedIn </a>\n</nav>`;
+    dynamicTextCss = `.luizFernando {\nidade : 23anos;\nexperiencia: 1.5anos;\ncargo : frontEndDeveloper;\n}`
 
   tsView() {
     this.isHtmlOpen = false;
@@ -63,10 +46,6 @@ export class CodeEditorComponent implements AfterViewInit, OnInit {
     this.editorService.simulateTyping('editorTs', `${this.dynamicTextTs}` );
   }
 
-
-
-
-
   htmlView() :void {
       this.isTsOpen = false;
       this.isCssOpen = false;
@@ -74,7 +53,6 @@ export class CodeEditorComponent implements AfterViewInit, OnInit {
       this.editorHtml = this.editorService.createEditor('editorHtml');
       this.editorService.simulateTyping('editorHtml', `${this.dynamicTextHTML}` );
   }
-
 
   cssView() :void {
     this.isTsOpen = false;
